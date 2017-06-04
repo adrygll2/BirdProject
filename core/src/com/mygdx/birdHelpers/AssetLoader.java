7bb15b31e654d922d46cc676152c4af82a464a95
adrygll2 <n17adri@gmail.com>
2017-06-04 17:26:49 +0200

    AssetLoader Created.

---
+++ core/src/com/mygdx/birdHelpers/AssetLoader.java
@@ -0,0 +1,22 @@
+package com.mygdx.birdHelpers;
+
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class AssetLoader {
+
+    public static Texture texture;
+    public static TextureRegion background, grass;
+
+    public static Animation birdAnimation;
+    public static TextureRegion bird, birdUp, birdDown;
+
+    public static TextureRegion pipeUp, pipeDown, bar;
+
+    public static void load(){
+
+    }
+
+}

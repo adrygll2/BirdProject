479be27c6d5c5467da29ca0b8425a28c16b67212
adrygll2 <n17adri@gmail.com>
2017-06-04 10:33:10 +0200

    Blue Screen

---
+++ html/src/com/mygdx/birdgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.birdgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.birdgame.BirdGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new BirdGame();
+        }
+}
